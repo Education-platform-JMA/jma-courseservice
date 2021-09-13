6ba3d489e94cd6b3479fc58b7a48880580471e06
fapus <m.cherevko@andersenlab.com>
2021-09-13 12:19:47 +0300

    Enabled Discovery Client

--- src/main/java/ru/jma/jmacourseservice/JmaCoursesServiceApplication.java
+++ src/main/java/ru/jma/jmacourseservice/JmaCoursesServiceApplication.java
@@ -2,8 +2,10 @@ package ru.jma.jmacourseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JmaCoursesServiceApplication {
 
     public static void main(String[] args) {
