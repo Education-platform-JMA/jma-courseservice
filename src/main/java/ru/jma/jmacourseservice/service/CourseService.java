993d9e9c339ed0be9816f5cd2303ac79597532a8
Тимофей Шошин <timofeyshoshin@icloud.com>
2021-09-16 23:18:26 +0300

    created a ReactiveMongoDB Rest CRUD API

---
+++ src/main/java/ru/jma/jmacourseservice/service/CourseService.java
@@ -0,0 +1,51 @@
+package ru.jma.jmacourseservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ru.jma.jmacourseservice.model.Course;
+import ru.jma.jmacourseservice.repository.CourseRepository;
+
+@RequiredArgsConstructor
+@Service
+public class CourseService {
+    private final CourseRepository courseRepository;
+
+    public Flux<Course> getAllCourses() {
+        return this.courseRepository.findAll();
+    }
+
+    public Mono<Course> getCourseById(final String id) {
+        return this.courseRepository.findById(id);
+    }
+
+    public Mono<Course> saveCourse(final Course course) {
+        return this.courseRepository.save(course);
+    }
+
+    public Mono<Course> updateCourse(final Course course) {
+        return this.courseRepository.findById(course.getId())
+                .map(p -> course)
+                .flatMap(this.courseRepository::save);
+    }
+
+    public Mono<Void> deleteCourse(final String id) {
+        return this.courseRepository.deleteById(id);
+    }
+
+    public Mono<Void> deleteAllCourses() {
+        return this.courseRepository.deleteAll();
+    }
+
+    public Flux<Course> findByPublished(boolean published) {
+        return this.courseRepository.findByPublished(published);
+    }
+
+    public Flux<Course> findByTitleContaining(final String title) {
+        return this.courseRepository.findByTitleContaining(title);
+    }
+
+
+}
+
