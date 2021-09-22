f41095b625403ebcab7d14aae1a406e1c8619d3b
Sergey Melnikov <74730932+pdasilem@users.noreply.github.com>
2021-09-22 11:58:20 +0500

    Delete ClassLib.java
    my fault

--- src/main/java/ru/education_platform_jma/jmacourseservice/ClassLib.java
+++
@@ -1,9 +0,0 @@
-package ru.jma.jmalib;
-
-// you should add needed functionality to the class (module)
-public class ClassLib {
-
-    public static void main(String[] args) {
-        System.out.println("it works!");
-    }
-}
